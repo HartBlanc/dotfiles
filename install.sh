005c3ffa25ecb3b31a69ebe010223fab06c26cba
callum <callum@thoughtmachine.net>
2022-03-31 22:21:58 +0100

    update installers

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ xcode-select --install
 # Clone repo into new hidden directory
 git clone https://github.com/HartBlanc/dotfiles.git ~/.dotfiles
 
-# install homebrew
+# Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --file=~/.dotfiles/Brewfile
---
+++ install_ubuntu.sh
@@ -0,0 +1,28 @@
+# install latest version of tmux as an appimage
+curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
+| grep "browser_download_url.*appimage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi - \
+&& chmod u+x tmux.appimage \
+&& mv tmux.appimage /usr/local/bin/tmux
+
+# install cargo
+curl https://sh.rustup.rs -sSf | sh
+
+# install rust packages to $HOME/.cargo/bin
+pkgs="exa fd-find bat ripgrep sd"
+for pkg in $pkgs; do
+    cargo install $pkg
+done
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# install neovim
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
+&& chmod u+x nvim.appimage \
+&& mv nvim.appimage /usr/lcoal/bin/tmux
+
+chsh -s $(which zsh)
