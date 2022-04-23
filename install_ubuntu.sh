0a61c536854ecc1e5a0572df39d2f54ba1a606f7
Callum Hart <github@callumhart.me>
2022-04-23 23:47:04 +0100

    refactor apt gets

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,8 +1,19 @@
 sudo apt get update
 sudo apt get upgrade
 
-# FUSE is required for appimages
+# required for appimages
 sudo apt install fuse
+
+# required for sumneko language server and also for general decompressing
+sudo apt install unzip
+# required for some neovim lsp lanaguage servers
+sudo apt install npm
+
+# gcc toolchain so we can compile rust binaries
+sudo apt install build-essential
+
+# required to link dotfiles in and just so we have the shell...
+sudo apt install zsh
 
 # install latest version of tmux as an appimage
 curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
@@ -19,9 +30,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # install cargo
 curl https://sh.rustup.rs -sSf | sh
 
-# install gcc toolchain so we can compile rust binaries
-sudo apt install build-essential
-
 # install rust binaries to $HOME/.cargo/bin
 cargo install exa fd-find bat ripgrep sd git-delta stylua
 
@@ -32,9 +40,6 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 # install zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
-# required for some lsp language servers
-sudo apt install npm unzip
-
 # install neovim
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage \
 && chmod u+x nvim.appimage \
@@ -43,6 +48,5 @@ curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimag
 # install starship prompt
 curl -sS https://starship.rs/install.sh | sh
 
-sudo apt install zsh
 zsh ~/.dotfiles/link.zsh
 chsh -s $(which zsh)
