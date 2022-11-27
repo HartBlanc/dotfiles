80ecb4aef8a2d314254e32e668b7c95cdb13b465
callum <notify+github@callumhart.me>
2022-11-27 12:05:14 +0000

    some vifm mappings

--- config/nvim/lua/plugins/configs/fm-nvim.lua
+++ config/nvim/lua/plugins/configs/fm-nvim.lua
@@ -11,8 +11,18 @@ local function chg_cwd()
 end
 
 local function mappings()
-	-- write cwd to /tmp/vifm before exiting vifm so that it can be read by on_close
+	-- write cwd to /tmp/vifm before exiting vifm so that it can be read by on_close to change the directory
 	vim.api.nvim_buf_set_keymap(0, "t", "<c-g>", ":execute ':!echo %d > /tmp/vifm'<cr>:q<cr>", { silent = true })
+
+	-- make ctrl-v close the file manager and open the file in a split pane. The default behaviour is to only
+	-- open the split pane once the file manager has been closed manually by the user.
+	vim.api.nvim_buf_set_keymap(
+		0,
+		"t",
+		"<c-v>",
+		'<C-\\><C-n>:lua require("fm-nvim").setMethod("vsplit | edit")<CR>il<cr>',
+		{ silent = true }
+	)
 end
 
 fm_nvim.setup({
@@ -21,5 +31,10 @@ fm_nvim.setup({
 	},
 	on_open = {
 		mappings,
+	},
+	mappings = {
+		-- something that I'm unlikely to press so that it is effectively disabled (these mappings are set after on_open
+		-- so it would override the <c-v> defined in mappings if we didn't set it)
+		vert_split = "sirerofpuwyq-;ufwyq-;pufyqw-;pufyqw-;",
 	},
 })
