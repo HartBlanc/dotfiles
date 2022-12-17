acf5a80b6cdfbb80a948b65d28136fa818a23641
callum <notify+github@callumhart.me>
2022-12-17 19:50:42 +0000

    change cmp mappings

--- config/nvim/lua/plugins/configs/nvim-cmp.lua
+++ config/nvim/lua/plugins/configs/nvim-cmp.lua
@@ -20,15 +20,15 @@ cmp.setup({
 	mapping = {
 		["<c-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<c-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-		["<c-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<c-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		["<c-space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }), -- get autocompletion suggestions
+		-- ["<c-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<c-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		["<tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i" }),
-		["<s-tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i" }),
-		["<cr>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<c-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i" }),
+		["<c-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i" }),
+		["<c-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. `select = true` implicity confirms the item when it is selected. To explicitly remove the item you must abort.
 	},
 })
 
